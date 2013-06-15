02c161800c237623f668858bcf44a392945cb2e7
reuben <reuben@reubens-Mac.local>
2013-06-15 16:39:40 -0700

    Initial Commit

---
+++ crosswordcontextualsolverTests/crosswordcontextualsolverTests.h
@@ -0,0 +1,13 @@
+//
+//  crosswordcontextualsolverTests.h
+//  crosswordcontextualsolverTests
+//
+//  Created by reuben on 15/6/13.
+//  Copyright (c) 2013 space.works. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface crosswordcontextualsolverTests : SenTestCase
+
+@end
---
+++ crosswordcontextualsolverTests/crosswordcontextualsolverTests.m
@@ -0,0 +1,32 @@
+//
+//  crosswordcontextualsolverTests.m
+//  crosswordcontextualsolverTests
+//
+//  Created by reuben on 15/6/13.
+//  Copyright (c) 2013 space.works. All rights reserved.
+//
+
+#import "crosswordcontextualsolverTests.h"
+
+@implementation crosswordcontextualsolverTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in crosswordcontextualsolverTests");
+}
+
+@end
